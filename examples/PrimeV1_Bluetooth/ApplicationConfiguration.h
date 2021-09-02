cb2e7362d91bd834f48691a2367e120c9593321f
Orlin Dimitrov <orlin369@gmail.com>
2021-09-02 16:31:26 +0300

    Remove unused definitions

--- examples/PrimeV1_Bluetooth/ApplicationConfiguration.h
+++ examples/PrimeV1_Bluetooth/ApplicationConfiguration.h
@@ -9,49 +9,6 @@
 	#include "WProgram.h"
 #endif
 
-#pragma region GPIO Map
-
-#define PIN_LS_1 8
-#define PIN_LS_2 9
-#define PIN_LS_3 10
-#define PIN_LS_4 11
-#define PIN_LS_5 12
-#define PIN_LS_6 13
-#define PIN_LS_7 14
-#define PIN_LS_8 15
-
-
-#define PIN_LEFT_ENCODER 2
-
-#define PIN_RIGHT_ENCODER 3
-
-
-#define PIN_LEFT_SPEED 5
-
-#define PIN_RIGHT_SPEED 6
-
-#define PIN_LEFT_DIRECTION 4
-
-#define PIN_RIGHT_DIRECTION 7
-
-
-#define PIN_US_SERVO 9
-
-#define PIN_US_TRIG 10
-
-#define PIN_US_ECHO 11
-
-
-#define PIN_USER_BUZZER 8
-
-#define PIN_USER_BUTTON 12
-
-#define PIN_USER_LED 13
-
-#define PIN_THROTLE A3
-
-#pragma endregion
-
 #pragma region Communication Port
 
 #define COM_PORT Serial
